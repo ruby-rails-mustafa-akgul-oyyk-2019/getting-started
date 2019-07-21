207f22bd0730d789117565a8e4b0c1a679135880
boratanrikulu <boratanrikulu@gmail.com>
2019-07-21 12:31:16 +0300

    Update action is added for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,15 @@
 class ArticlesController < ApplicationController
-  def index
+  before_action :set_article, only: [ :show, :edit, :update ]
+
+  def index #GET
     @articles = Article.all
   end
 
-  def new
+  def new # GET
     @article  = Article.new
   end
   
-  def create
+  def create # POST
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
@@ -16,13 +18,27 @@ class ArticlesController < ApplicationController
     end
   end
   
-  def show
-    @article = Article.find(params[:id])
+  def show # GET
+  end
+
+  def edit # GET
+  end
+
+  def update # POST
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
   
   private
   
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
